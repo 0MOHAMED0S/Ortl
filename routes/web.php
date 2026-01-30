75552c0a24112fa705cb91b38a603e25962d81ac
mohamed <msa0back@gmail.com>
2026-01-30 06:13:05 +0200

    h

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 Route::get('/teacher', function () {
     return view('main.teacher');
 })->name('teacher.index');
