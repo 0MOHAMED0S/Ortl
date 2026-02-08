3734ba8842ac56bec06838199d99e8ce0da13aaf
mohamed <msa0back@gmail.com>
2026-02-08 10:59:07 +0200

    b

--- resources/views/dashboard/layouts/master.blade.php
+++ resources/views/dashboard/layouts/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>لوحة التحكم - ورتل</title>
+<link rel="icon" href="{{ asset('images/mainlogo.png') }}" type="image/png">
 
     {{-- Bootstrap & Fonts --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css">
