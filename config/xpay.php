86ab17ae9e75e4d5108e05f4688d59a006ca65cc
mohamed <msa0back@gmail.com>
2026-08-29 23:06:26 +0300

    Update XPay configuration securely

--- config/xpay.php
+++ config/xpay.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'secret_key'      => env('XPAY_SECRET_KEY', 'sk_test_fcjDFmwJ82lJkW3OXil8Vasklwa7zswM'),
-    'publishable_key' => env('XPAY_PUBLISHABLE_KEY', 'pk_test_RCSFF3qO89FPvez46ztaCqCJwwFKvsK'),
-    'webhook_secret'  => env('XPAY_WEBHOOK_SECRET', 'whsec_EgdMDpBdFFyeytyhT3Npmx59skus0HK8'),
+    'secret_key'      => env('XPAY_SECRET_KEY', ''),
+    'publishable_key' => env('XPAY_PUBLISHABLE_KEY', ''),
+    'webhook_secret'  => env('XPAY_WEBHOOK_SECRET', ''),
     'base_url'        => env('XPAY_BASE_URL', 'https://api.xpay.app/'),
 ];
+
